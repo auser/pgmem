e085e1d9fd20861dd10e49357946bea30ce42f8c
Ari <me@ari.io>
2022-11-08 09:58:54 -0500

    Updated to use typescript for typing

--- src/system/mod.rs
+++ src/system/mod.rs
@@ -15,9 +15,9 @@ fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
 
 pub fn neon_main(mut cx: ModuleContext) -> NeonResult<()> {
     cx.export_function("hello", hello)?;
-    cx.export_function("init", SystemServer::js_new)?;
-    cx.export_function("start", SystemServer::js_start)?;
-    cx.export_function("stop", SystemServer::js_stop)?;
+    cx.export_function("init_db", SystemServer::js_new)?;
+    cx.export_function("start_db", SystemServer::js_start)?;
+    cx.export_function("stop_db", SystemServer::js_stop)?;
     cx.export_function("new_db", SystemServer::js_create_new_db)?;
     cx.export_function("drop_db", SystemServer::js_drop_database)?;
     // cx.export_function("start_db", SystemServer::js_start_db)?;
